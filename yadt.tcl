cdf9925cd93dfb7b5ba9a617d6d5063f1a35fda8
Alexander Sokolov <avsokol@gmail.com>
2014-10-01 20:04:39 +0400

    version increased

--- yadt.tcl
+++ yadt.tcl
@@ -1940,7 +1940,7 @@ proc ::Yadt::Run {} {
     variable ::Yadt::DIFF_FILES
 
     set Revision ""
-    set CVS_REVISION [ lindex [ split "$Revision: 3.254 $" ] 1 ]
+    set CVS_REVISION [ lindex [ split "$Revision: 3.256 $" ] 1 ]
 
     set OPTIONS(is_starkit) 0
     if { ![ catch { package present starkit } ] && [ info exists ::starkit::topdir ] } {
