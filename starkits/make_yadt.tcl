9df17a1c5666e0cbd99d775ccca1f6e1a23c46e2
Alexander Sokolov <avsokol@gmail.com>
2014-12-20 17:47:29 +0300

    fix toolsrc

--- starkits/make_yadt.tcl
+++ starkits/make_yadt.tcl
@@ -66,10 +66,26 @@ proc ::MakeYadt::Get_Common_Files_List { syren_src_dir } {
 
 proc ::MakeYadt::Get_Platform_Specific_Files_List { platform syren_src_dir } {
 
-    set src_files_list [ list cvs diff ]
-    if { $platform == "windows" } {
-        set src_files_list [ list cvs.exe diff.exe ]
+    set fname .toolsrc
+    set rcfile [ file join $syren_src_dir $fname ]
+
+    set content {}
+    set src_files_list {}
+
+    foreach utility [ list cvs diff ] {
+        set exe_name $utility
+        if { $platform == "windows" } {
+            set exe_name ${utility}.exe
+        }
+        lappend src_files_list $exe_name
+        lappend content [ list $utility $exe_name ]
     }
+
+    set fd [ open $rcfile w+ ]
+    puts $fd [ join $content \n ]
+    close $fd
+
+    lappend src_files_list $fname
 
     return $src_files_list
 }
